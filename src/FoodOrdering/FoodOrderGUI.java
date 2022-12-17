73e3b56486938f441598c61cd61576bf4e0a52f7
Rhyss <rhyssgianne123@gmail.com>
2022-12-17 23:03:30 +0800

    Rename FoodOrder components

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -2,7 +2,7 @@ package FoodOrdering;
 
 import javax.swing.*;
 
-public class FoodOrderGUI {
+public class FoodOrderGUI extends JFrame{
     private JPanel panel1;
     private JCheckBox pizzaCheckBox;
     private JRadioButton noneRadioButton;
